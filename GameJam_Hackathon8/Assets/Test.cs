c4d4d3723630473e9fcc1c7c168ae348c7b5d7b1
Pruthvi <pruthvi@users.noreply.github.com>
2019-02-25 13:54:13 -0500

    Updated Video Player script

---
+++ GameJam_Hackathon8/Assets/Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+
+public class Test : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+
+    public VideoPlayer vp;
+    public GameObject quad;
+    private TrackableBehaviour mTrackableBehaviour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = quad.GetComponent<VideoPlayer>();
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            Debug.Log("Video Playing");
+            vp.Play();
+        }
+        else
+        {
+            Debug.Log("Video Playing");
+
+            // Stop audio when target is lost
+            vp.Stop();
+        }
+    }
+}
